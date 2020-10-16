b2e7d803252a024bce39e2c9d48f508d936bbed7
pouyarezvani1 <pouyarezvani10@gmail.com>
2020-10-15 19:40:29 -0700

    First push

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import PropTypes from 'pr'
+
+export class Navbar extends Component {
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: 'fab fa-github',
+    }
+    render() {
+        return (
+            <div className="navbar bg-primary">
+                <h1> <i className={this.props.icon}></i> {this.props.title}</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar
