6f1791e7ae8b9dff4f70027ac47dc8c79dbb664a
pouyarezvani <pouyarezvani10@gmail.com>
2020-10-22 13:01:48 -0700

    new Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
-    render(){
+    render() {
         return (
             <div className="App">
-                <Navbar title="Github Finder" icon="fab fa-github"/>
+                <Navbar />
+                <div className="container">
+                    <Users />
+                </div>
             </div>
         );
     }
